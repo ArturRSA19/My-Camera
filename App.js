7e1fa2db0c12a6fe64d19494f60197a08dffaa68
Artur Rodrigues <artur190503@outlook.com>
2023-11-27 14:56:02 -0300

    Colocando botões para tirar foto e inverter câmera

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
 
+  const camRef = useRef(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [hasPermission, setHasPermission] = useState(null);
+  const [capturedPhoto, setCapturedPhoto] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -21,13 +24,42 @@ export default function App() {
     return <Text>Acesso negado!</Text>;
   }
 
+  async function takePicture(){
+    if(camRef){
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri);
+      console.log(data);
+    }
+  }
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Camera
         style={styles.camera}
         type={type}
-      />
-    </View>
+      >
+      <View style={styles.contentButtons}>
+        <TouchableOpacity 
+          style={styles.buttonFlip}
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+            );
+          }}
+        >
+          <FontAwesome name="exchange" size={23} color="#fff" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonCamera}
+          onPress={takePicture}
+        >
+          <FontAwesome name="camera" size={23} color="#fff" />
+        </TouchableOpacity>
+      </View>
+      </Camera>
+    </SafeAreaView>
   );
 }
 
@@ -39,5 +71,36 @@ const styles = StyleSheet.create({
   camera: {
     width: '100%',
     height: '100%',
+  },
+  contentButtons: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 20,
+  },
+  buttonFlip: {
+    position: 'absolute',
+    bottom: 50,
+    left: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+  buttonCamera: {
+    position: 'absolute',
+    bottom: 50,
+    right: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
   }
 });
